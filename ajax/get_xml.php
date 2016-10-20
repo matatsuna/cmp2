11519778f41ec4eb3d31a1c2163f663e677ad8e0
matatsuna <ev50510@meiji.ac.jp>
2016-10-20 14:29:25 +0900

    first

---
+++ ajax/get_xml.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_GET["url"]) && preg_match("/^https?:/",$_GET["url"])){
+  header('Content-type: application/xml');
+  $req_url = $_GET['url'];
+  foreach ( $_GET as $key => $value){
+    if( strcmp( $key, "url" ) ){
+      // url でない場合はオプションをつなげる
+      $req_url .= ("&" . $key . "=" . $value);
+    }
+  }
+  echo file_get_contents($req_url);
+}else{
+  echo "error";
+}
+?>

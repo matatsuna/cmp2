24f72e3176794c3e3868f1eeeb4ead2ecf61fbf5
matatsuna <ev50510@meiji.ac.jp>
2016-10-22 11:46:03 +0900

    fix ajax
    ajaxの一部を修正

--- ajax/main.js
+++ ajax/main.js
@@ -9,7 +9,7 @@ $(function(){
                     $("#title").html('<h1>'+data.title+'</h1>');
                     $("#today").html('<h3>'+data.forecasts[0].date+'</h3>'+'<p>'+data.forecasts[0].telop+'</p>');
                     $("#link").html('<a href="'+data.pinpointLocations[13].link+'">'+data.pinpointLocations[13].name+'</a>');
-                    $("#link").html('<p>'+data.description.text+'</p>');
+                    $("#text").html('<p>'+data.description.text+'</p>');
         }
     });
 });
